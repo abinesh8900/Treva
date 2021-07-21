2cd34071f522fd566d5a711f493bf4d9bf8d8b83
abinesh8900 <abineshranganathan@gmail.com>
2021-07-22 01:08:02 +0530

    validation signup

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// ******signup popup*******//
 const signPopup = document.getElementById("signup-popup");
 const overlay = document.getElementById("overlay");
 const openSignup = document.getElementById("signup-btn");
@@ -16,6 +17,7 @@ openSignup.addEventListener("click", openSignupPopup);
 closeSignup.addEventListener("click", closeSignupPopup);
 overlay.addEventListener("click", closeSignupPopup);
 
+// *******signup validation******//
 const signupForm = document.getElementById("signup-form");
 const fullName = document.getElementById("fullname");
 const userName = document.getElementById("username");
@@ -23,20 +25,27 @@ const email = document.getElementById("email");
 const phone = document.getElementById("phone");
 const password = document.getElementById("password");
 const confrimPassword = document.getElementById("confrimPassword");
+const temrsAndConditions = document.getElementById("terms-conditions");
 const cancelSignup = document.getElementById("signupCancel");
-const signupInput = signPopup.querySelector(".field-control");
-console.log(signupInput);
+const signupInput = signPopup.querySelectorAll(".field-control");
 
 signupForm.addEventListener("submit", function (event) {
   event.preventDefault();
   checkInputs();
 });
-cancelSignup.addEventListener("click", function () {
-  signupInput.classList.remove("error");
-  signupInput.classList.remove("success");
-});
+
+for (const items of signupInput) {
+  cancelSignup.addEventListener("click", function () {
+    items.classList.remove("error");
+    items.classList.remove("success");
+  });
+}
 
 function checkInputs() {
+  const emailFormat =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const phoneNo = /^\d{10}$/;
+
   const fullNameValue = fullName.value.trim();
   const userNameValue = userName.value.trim();
   const emailValue = email.value.trim();
@@ -55,15 +64,27 @@ function checkInputs() {
   } else {
     setSuccessFor(userName);
   }
+
   if (emailValue === "") {
     setErrorFor(email, "Email can't be blank");
-  } else {
+    // console.log("is blank");
+  } else if (emailValue.match(emailFormat)) {
     setSuccessFor(email);
+    // console.log("is a valid");
+  } else {
+    console.log("not valid");
+    setErrorFor(email, "Email is not valid ");
   }
+
   if (phoneValue === "") {
     setErrorFor(phone, "Phone can't be blank");
-  } else {
+    // console.log("is blank");
+  } else if (phoneValue.match(phoneNo)) {
     setSuccessFor(phone);
+    // console.log("is a valid");
+  } else {
+    setErrorFor(phone, "Phone number is not valid");
+    // console.log("not valid");
   }
   if (passwordValue === "") {
     setErrorFor(password, "Password can't be blank");
@@ -71,9 +92,19 @@ function checkInputs() {
     setSuccessFor(password);
   }
   if (confrimPasswordValue === "") {
+    // console.log("is blank");
     setErrorFor(confrimPassword, "Password Confrim can't be blank");
-  } else {
+  } else if (passwordValue === confrimPasswordValue) {
     setSuccessFor(confrimPassword);
+    // console.log("match");
+  } else {
+    setErrorFor(confrimPassword, "Password not match");
+    // console.log(" not match");
+  }
+  if (temrsAndConditions.checked) {
+    setSuccessFor(temrsAndConditions);
+  } else {
+    setErrorFor(temrsAndConditions, "Select the check box");
   }
 }
 
